a9d48017f1bac8e0a5cdbd8d4f2f7713661759ef
greathouk <tylergames251@gmail.com>
2021-11-07 22:23:19 -0500

    Small Formating

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ pub fn main() -> Result<(), String> {
 
     // Make a new thread to handle the drawing logic in logic.rs
 
-    // Make a new thread to handle and process all events, 
+    // Make a new thread to handle and process all events,
     // Sending the data off to the required threads, covered in events.rs
 
     // Start the event loop and pass the events to the proper threads,
@@ -23,17 +23,17 @@ pub fn main() -> Result<(), String> {
 
     // Triangle of messaging
     /*
-                  Events with
-                  Drawing Impacts
-        logic.rs <------- events.rs
-           \                  >
-            \               /
-   Updated   \            /   All Events
-   Frames     \         /
-               >      /
-                main.rs
+                   Events with
+                   Drawing Impacts
+         logic.rs <------- events.rs
+            \                  >
+             \               /
+    Updated   \            /   All Events I want to handle
+    Frames     \         /
+                >      /
+                 main.rs
 
-    */
+     */
 
     Ok(())
 }
