2cab35f2e11a50dc9f257858cb2cd00f9ec3645b
greathouk <tylergames251@gmail.com>
2021-11-08 22:01:34 -0500

    A Lot Of Work On main.rs And windows.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 extern crate sdl2;
+mod events;
 mod windows;
 
+use sdl2::event::{Event, WindowEvent};
+use sdl2::keyboard::Keycode;
 use sdl2::video::WindowPos;
-use windows::graphing_window;
+use std::time::Instant;
+use windows::GraphingWindow;
 
 pub fn main() -> Result<(), String> {
     // Call setup functions for sdl2
@@ -10,7 +14,7 @@ pub fn main() -> Result<(), String> {
     let video_subsystem = sdl_context.video()?;
 
     // Call Main Window Init and Open Function from windows.rs
-    let main_window = graphing_window::init(
+    let mut main_window = GraphingWindow::init(
         video_subsystem,
         "Main Window",
         300,
@@ -30,6 +34,43 @@ pub fn main() -> Result<(), String> {
 
     // Start the event loop and pass the events to the proper threads,
     // As well as wait for any draw requests from the drawing thread
+    let mut event_pump = sdl_context.event_pump().map_err(|e| e.to_string())?;
+    'running: loop {
+        let now = Instant::now();
+
+        // Handle Events
+        for event in event_pump.poll_iter() {
+            match event {
+                // If sdl2 wants to quite or escape is pressed,
+                // Then quit
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                // If a window resizes, then we need to tell it
+                Event::Window {
+                    win_event: WindowEvent::Resized(wid, hei),
+                    window_id: id,
+                    ..
+                } => {
+                    if id == main_window.window().id() {
+                        main_window.resized(wid as u32, hei as u32)?;
+                    } else {
+                        return Err("Window Resize Event Fail".to_string());
+                    }
+                }
+                // Default: Send event off to the event thread
+                _ => {}
+            }
+        }
+
+        // See if there are any frames to grab
+        // If so, copy and present it
+
+        let framerate = 1000000. / now.elapsed().as_micros() as f64;
+        println!("Framerate: {}", framerate);
+    }
 
     // Triangle of messaging
     /*
--- src/windows.rs
+++ src/windows.rs
@@ -1,68 +1,139 @@
 // All Used Windows
+pub use self::graphing_window::GraphingWindow;
 
-use sdl2::render::{Canvas, TextureCreator};
-use sdl2::video::{Window, WindowContext};
+mod basic_window {
+    use sdl2::render::{Canvas, TextureCreator};
+    use sdl2::video::{Window, WindowContext, WindowPos};
 
-struct SDL2Window {
-    canvas: Canvas<Window>,
-    texture_creator: TextureCreator<WindowContext>,
+    pub struct BasicWindow {
+        canvas: Canvas<Window>,
+        texture_creator: TextureCreator<WindowContext>,
+    }
+    impl BasicWindow {
+        pub fn init(
+            video_subsystem: sdl2::VideoSubsystem,
+            title: &'static str,
+            width: u32,
+            min_width: Option<u32>,
+            max_width: Option<u32>,
+            height: u32,
+            min_height: Option<u32>,
+            max_height: Option<u32>,
+            posx: WindowPos,
+            posy: WindowPos,
+            resizable: bool,
+            hidden: bool,
+            borderless: bool,
+            fullscreen: bool,
+        ) -> Result<BasicWindow, String> {
+            let mut window = {
+                let mut win = video_subsystem.window(title, width, height);
+                win.position(
+                    BasicWindow::to_ll_windowpos(posx),
+                    BasicWindow::to_ll_windowpos(posy),
+                );
+                if resizable {
+                    win.resizable();
+                }
+                if hidden {
+                    win.hidden();
+                }
+                if borderless {
+                    win.borderless();
+                }
+                if fullscreen {
+                    win.fullscreen();
+                }
+                win.build().map_err(|e| e.to_string())?
+            };
+            match (min_width, min_height) {
+                (Some(wid), Some(hei)) => window.set_minimum_size(wid, hei),
+                (None, Some(hei)) => window.set_minimum_size(0, hei),
+                (Some(wid), None) => window.set_minimum_size(wid, 0),
+                (None, None) => Ok(()),
+            }
+            .map_err(|e| e.to_string())?;
+            match (max_width, max_height) {
+                (Some(wid), Some(hei)) => window.set_maximum_size(wid, hei),
+                (None, Some(hei)) => window.set_maximum_size(0, hei),
+                (Some(wid), None) => window.set_maximum_size(wid, 0),
+                (None, None) => Ok(()),
+            }
+            .map_err(|e| e.to_string())?;
+            let canvas = window
+                .into_canvas()
+                .present_vsync()
+                .build()
+                .map_err(|e| e.to_string())?;
+            let texture_creator = canvas.texture_creator();
+            Ok(BasicWindow {
+                canvas,
+                texture_creator,
+            })
+        }
+        pub fn window(&mut self) -> &mut Window {
+            self.canvas.window_mut()
+        }
+        fn to_ll_windowpos(pos: WindowPos) -> i32 {
+            match pos {
+                WindowPos::Undefined => sdl2_sys::SDL_WINDOWPOS_UNDEFINED_MASK as i32,
+                WindowPos::Centered => sdl2_sys::SDL_WINDOWPOS_CENTERED_MASK as i32,
+                WindowPos::Positioned(x) => x as i32,
+            }
+        }
+    }
 }
 
-impl SDL2Window {
-    fn init(
-        video_subsystem: sdl2::VideoSubsystem,
-        title: &'static str,
-        width: u32,
-        height: u32,
-    ) -> Result<SDL2Window, String> {
-        let window = video_subsystem
-            .window(title, width, height)
-            .build()
-            .map_err(|e| e.to_string())?;
+mod graphing_window {
+    use super::basic_window::BasicWindow;
+    use sdl2::video::{Window, WindowPos};
 
-        let canvas = window
-            .into_canvas()
-            .present_vsync()
-            .build()
-            .map_err(|e| e.to_string())?;
-
-        let texture_creator = canvas.texture_creator();
-
-        Ok(SDL2Window {
-            canvas,
-            texture_creator,
-        })
+    pub struct GraphingWindow {
+        raw: BasicWindow,
     }
 
-    fn window(&mut self) -> &mut Window {
-        self.canvas.window_mut()
-    }
-}
+    impl GraphingWindow {
+        pub fn init(
+            video_subsystem: sdl2::VideoSubsystem,
+            title: &'static str,
+            width: u32,
+            height: u32,
+            posx: WindowPos,
+            posy: WindowPos,
+            // iconPath: Option<>
+        ) -> Result<GraphingWindow, String> {
+            let window = BasicWindow::init(
+                video_subsystem,
+                title,
+                width,
+                Some(500),
+                None,
+                height,
+                Some(500),
+                None,
+                posx,
+                posy,
+                true,
+                false,
+                false,
+                false,
+            )?;
 
-pub mod graphing_window {
-    use super::SDL2Window;
-    use sdl2::video::WindowPos;
-
-    pub struct Window {
-        raw: SDL2Window,
-    }
-
-    pub fn init(
-        video_subsystem: sdl2::VideoSubsystem,
-        title: &'static str,
-        width: u32,
-        height: u32,
-        posx: WindowPos,
-        posy: WindowPos,
-        // iconPath: Option<>
-    ) -> Result<Window, String> {
-        let mut window = SDL2Window::init(video_subsystem, title, width, height)?;
-        {
-            let win = window.window();
-            win.set_position(posx, posy);
-            win.hide();
+            Ok(GraphingWindow { raw: window })
         }
 
-        Ok(Window { raw: window })
+        pub fn resized(&mut self, width: u32, height: u32) -> Result<(), String> {
+            let size = (width as f32 * height as f32).sqrt() as u32;
+
+            self.window()
+                .set_size(size, size)
+                .map_err(|e| e.to_string())?;
+
+            Ok(())
+        }
+
+        pub fn window(&mut self) -> &mut Window {
+            self.raw.window()
+        }
     }
 }
